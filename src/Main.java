e007b019ca723161086cff5a6c8a20dfbeee5530
masih abedini <masih7816@gmail.com>
2024-06-30 22:19:19 +0330

    add some Tree class's method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tree s = new Tree();
+//        s.printTree();
+        s.printSideways();
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Tree {
@@ -70,6 +72,210 @@ public class Tree {
             p = s.pop();
             System.out.println(p.data + " ");
             p = p.right;
+        }
+    }
+
+    public void nonRecursivePreOrder() {
+        java.util.Stack<TreeNode> s = new Stack<>();
+        TreeNode p = root;
+        while (true) {
+            while (p != null) {
+                System.out.println(p.data + " ");
+                s.push(p);
+                p = p.left;
+            }
+            if (s.isEmpty()) {
+                return;
+            }
+            p = s.pop();
+            p = p.right;
+        }
+    }
+
+    public void levelOrder() {
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode p = root;
+        while (p != null) {
+            System.out.print(p.data + " ");
+            if (p.left != null) {
+                q.offer(p.left);
+            }
+            if (p.right != null) {
+                q.offer(p.right);
+            }
+            p = q.poll();
+        }
+    }
+
+    public void incrementAllNodes() {
+        incrementAllNodes(root);
+    }
+
+    private void incrementAllNodes(TreeNode p) {
+        if (p != null) {
+            p.data++;
+            incrementAllNodes(p.left);
+            incrementAllNodes(p.right);
+        }
+    }
+
+    public int numberOfNodes() {
+        return numberOfNodes(root);
+    }
+
+    private int numberOfNodes(TreeNode p) {
+        if (p == null) {
+            return 0;
+        }
+        return 1 + numberOfNodes(p.left) + numberOfNodes(p.right);
+    }
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(TreeNode p) {
+        if (p == null) {
+            return -1;
+        }
+        int m = height(p.left);
+        int n = height(p.right);
+        return m > n ? m + 1 : n + 1;
+    }
+
+    public Tree(TreeNode original) {
+        root = copy(original);
+    }
+
+    private TreeNode copy(TreeNode p) {
+        if (p == null) {
+            return null;
+        }
+        TreeNode temp = new TreeNode();
+        temp.data = p.data;
+        temp.left = copy(p.left);
+        temp.right = copy(p.right);
+        return temp;
+    }
+
+    private static boolean equals(TreeNode first, TreeNode second) {
+        if (first == null && second == null) {
+            return true;
+        }
+        if (first != null
+                && second != null
+                && first.data == second.data
+                && equals(first.left, second.left)
+                && equals(first.right, second.right)
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull() {
+        return isFull(root);
+    }
+
+    private boolean isFull(TreeNode p) {
+        if (p == null) {
+            return true;
+        }
+        if (height(p.left) != height(p.right)) {
+            return false;
+        }
+        return isFull(p.left) && isFull(p.right);
+    }
+
+    public boolean isStrict() {
+        return isStrict(root);
+    }
+
+    private boolean isStrict(TreeNode p) {
+        if (p == null) {
+            return true;
+        }
+        if (p.left == null && p.right != null) {
+            return false;
+        }
+        if (p.left != null && p.right == null) {
+            return false;
+        }
+
+        return isStrict(p.left) && isStrict(p.right);
+    }
+
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    private boolean isBalanced(TreeNode p) {
+        if (p == null) {
+            return true;
+        }
+        return (Math.abs(height(p.left) - height(p.right)) <= 1)
+                && isBalanced(p.left) && isBalanced(p.right);
+    }
+
+    public boolean isComplete() {
+        return isComplete(root);
+    }
+
+    private boolean isComplete(TreeNode p) {
+        int leftHeight, rightHeight;
+        boolean leftIsFull, rightIsFull, leftIsComplete, rightIsComplete;
+        if (p == null) {
+            return true;
+        }
+        leftHeight = height(p.left);
+        rightHeight = height(p.right);
+        leftIsFull = isFull(p.left);
+        rightIsFull = isFull(p.right);
+        leftIsComplete = isComplete(p.left);
+        rightIsComplete = isComplete(p.right);
+
+        if (leftIsFull && rightIsComplete && (leftHeight == rightHeight)) {
+            return true;
+        }
+        if (leftIsComplete && rightIsFull && (leftHeight == (rightHeight + 1))) {
+            return true;
+        }
+        return false;
+    }
+
+    public void printTree() {
+        printTree(root);
+    }
+
+    private void printTree(TreeNode p) {
+        if (p != null) {
+            if (p.left != null) {
+                System.out.print("(");
+            }
+            printTree(p.left);
+            if (p.left != null) {
+                System.out.print(")");
+            }
+            System.out.print(" " + p.data + " ");
+            if (p.right != null) {
+                System.out.print("(");
+            }
+            printTree(p.right);
+            if (p.right != null) {
+                System.out.print(")");
+            }
+        }
+    }
+
+    public void printSideways() {
+        printSideways(root, " ");
+    }
+
+    private void printSideways(TreeNode p, String indent) {
+        if (p != null) {
+            printSideways(p.right, indent + "    ");
+            System.out.println(indent + p.data);
+            printSideways(p.left, indent + "    ");
         }
     }
 }
